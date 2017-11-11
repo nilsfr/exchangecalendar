9de9063451ee54f5d54dcc70d3172e2787a04161
Adrien Dorsaz <adrien@adorsaz.ch>
2017-11-11 23:54:53 +0100

    emailtag: complete rename for emailtag file inside the overlay

--- emailtag/content/emailtag-overlay.js
+++ emailtag/content/emailtag-overlay.js
@@ -1,13 +1,13 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE 
-overlay SYSTEM "chrome://exchangecalendar/locale/rtews.dtd">
-<overlay id="rtews-overlay"
+overlay SYSTEM "chrome://exchangeemailtag/locale/emailtag.dtd">
+<overlay id="emailtag-overlay"
          xmlns="http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul">
 
     <script type="application/javascript"
-            src="chrome://exchangecalendar/content/rtews.js" />
+            src="chrome://exchangeemailtag/content/emailtag.js" />
     <stringbundleset id="stringbundleset">
         <stringbundle id="string-bundle"
-                      src="chrome://exchangecalendar/locale/rtews.properties" />
+                      src="chrome://exchangeemailtag/locale/emailtag.properties" />
     </stringbundleset>
 </overlay>
