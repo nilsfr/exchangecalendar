b886e40f1592ae2b2f6f2caa4b50d57bb3a3d6a6
Adrien Dorsaz <adrien@adorsaz.ch>
2018-02-08 22:59:54 +0100

    Port Mozilla bug 1387813 to exchanegcalendar

--- calendar/interface/exchangeTimeZones/mivExchangeTimeZones.js
+++ calendar/interface/exchangeTimeZones/mivExchangeTimeZones.js
@@ -423,7 +423,7 @@ mivExchangeTimeZones.prototype = {
     load_timezonedefinitions_file: function _load_timezonedefinitions_file() {
         var somefile = this.globalFunctions.chromeToPath("chrome://exchangeTimeZones/content/ewsTimesZoneDefinitions_2007.xml");
         var file = Components.classes["@mozilla.org/file/local;1"]
-            .createInstance(Components.interfaces.nsILocalFile);
+            .createInstance(Components.interfaces.nsIFile);
 
         file.initWithPath(somefile);
 
