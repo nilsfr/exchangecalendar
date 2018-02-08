d72211438e4dab2cb4603dc061058442350c1c16
Adrien Dorsaz <adrien@adorsaz.ch>
2018-02-09 00:18:16 +0100

    According to Lightning code, some resources are still located at resource:///
    That's the case for:
      * resource:///modules/mailServices.js
      * resource:///modules/iteratorUtils.jsm

--- addressbook/content/addressbookOverlay.js
+++ addressbook/content/addressbookOverlay.js
@@ -29,7 +29,7 @@ var components = Components;
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 Cu.import("resource://gre/modules/Services.jsm");
 
-Cu.import("resource://gre/modules/mailServices.js");
+Cu.import("resource:///modules/mailServices.js");
 
 Cu.import("resource://exchangecommon/ecFunctions.js");
 Cu.import("resource://exchangeaddress/exchangeAbFunctions.js");
--- addressbook/exchangeapi/exchangeAbFunctions.js
+++ addressbook/exchangeapi/exchangeAbFunctions.js
@@ -28,7 +28,7 @@ var components = Components;
 
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 Cu.import("resource://gre/modules/Services.jsm");
-Cu.import("resource://gre/modules/iteratorUtils.jsm");
+Cu.import("resource:///modules/iteratorUtils.jsm");
 
 Cu.import("resource://exchangecommon/ecFunctions.js");
 
